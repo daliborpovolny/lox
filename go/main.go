44aadcf097f56b6ed399edfa4628239d407fc100
daliborpovolny <dalibor.povolny@gmail.com>
2025-03-18 17:03:23 +0100

    add golang enum

--- go/main.go
+++ go/main.go
@@ -6,26 +6,60 @@ import (
 	"os"
 )
 
-type TokenType enum int{
+const (
 	// Single-character tokens.
-	LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
-	COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
-  
+	LEFT_PAREN = iota
+	RIGHT_PAREN
+
+	LEFT_BRACE
+	RIGHT_BRACE
+
+	COMMA
+	DOT
+	MINUS
+	PLUS
+	SEMICOLON
+	SLASH
+	STAR
+
 	// One or two character tokens.
-	BANG, BANG_EQUAL,
-	EQUAL, EQUAL_EQUAL,
-	GREATER, GREATER_EQUAL,
-	LESS, LESS_EQUAL,
-  
+	BANG
+	BANG_EQUAL
+
+	EQUAL
+	EQUAL_EQUAL
+
+	GREATER
+	GREATER_EQUAL
+
+	LESS
+	LESS_EQUAL
+
 	// Literals.
-	IDENTIFIER, STRING, NUMBER,
-  
+	IDENTIFIER
+	STRING
+	NUMBER
+
 	// Keywords.
-	AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
-	PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
-  
+	AND
+	CLASS
+	ELSE
+	FALSE
+	FUN
+	FOR
+	IF
+	NIL
+	OR
+	PRINT
+	RETURN
+	SUPER
+	THIS
+	TRUE
+	VAR
+	WHILE
+
 	EOF
-}
+)
 
 type Lox struct {
 	hadError bool
